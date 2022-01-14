5af85a2e146f5c38a72883830c6960be09cfec74
Alireza Zamani <alirezazamani2922@gmail.com>
2022-01-14 13:16:48 +0330

    init

---
+++ src/octokit.module.ts
@@ -0,0 +1,96 @@
+import { DynamicModule, Module, Provider } from '@nestjs/common';
+import { Octokit } from 'octokit';
+import {
+  OctokitModuleAsyncOptions,
+  OctokitModuleOptions,
+  OctokitOptionsFactory,
+} from './interfaces/octokit-module-options.interface';
+import { OCTOKIT, OCTOKIT_OPTIONS } from './octokit.constants';
+import { OctokitService } from './octokit.service';
+
+@Module({
+  providers: [OctokitService],
+  exports: [OctokitService],
+})
+export class OctokitModule {
+  public static forRoot(options: OctokitModuleOptions): DynamicModule {
+    return {
+      global: options.isGlobal || false,
+      module: OctokitModule,
+      providers: [
+        {
+          provide: OCTOKIT,
+          useValue: this.instantiateOctokit(options),
+        },
+      ],
+    };
+  }
+
+  public static forRootAsync(
+    options: OctokitModuleAsyncOptions
+  ): DynamicModule {
+    const OctokitProvider: Provider = {
+      useFactory: (options: OctokitModuleOptions) =>
+        this.instantiateOctokit(options),
+      provide: OCTOKIT,
+      inject: [OCTOKIT_OPTIONS],
+    };
+    return {
+      global: options.isGlobal,
+      imports: options.imports || [],
+      module: OctokitModule,
+      providers: [...this.createAsyncProviders(options), OctokitProvider],
+    };
+  }
+
+  private static createAsyncProviders(
+    optionsAsync: OctokitModuleAsyncOptions
+  ): Provider[] {
+    if (optionsAsync.useExisting || optionsAsync.useFactory) {
+      return [this.createAsyncOptionsProvider(optionsAsync)];
+    }
+    if (optionsAsync.useClass) {
+      return [
+        this.createAsyncOptionsProvider(optionsAsync),
+        {
+          provide: optionsAsync.useClass,
+          useClass: optionsAsync.useClass,
+        },
+      ];
+    }
+    throw Error(
+      'One of useClass, useFactory or useExisting should be provided'
+    );
+  }
+  private static createAsyncOptionsProvider(
+    options: OctokitModuleAsyncOptions
+  ): Provider {
+    if (options.useFactory) {
+      return {
+        provide: OCTOKIT_OPTIONS,
+        useFactory: options.useFactory,
+        inject: options.inject || [],
+      };
+    }
+
+    const provider: Provider = {
+      provide: OCTOKIT_OPTIONS,
+      useFactory: async (optionsFactory: OctokitOptionsFactory) =>
+        await optionsFactory.createOctokitOptions(),
+    };
+    if (options.useExisting) provider.inject = [options.useExisting];
+    if (options.useClass) provider.inject = [options.useClass];
+    return provider;
+  }
+
+  private static instantiateOctokit(options: OctokitModuleOptions): Octokit {
+    let MyOctokit = Octokit;
+
+    if (options.plugins) {
+      MyOctokit = MyOctokit.plugin(...options.plugins);
+      console.log('plugins applied');
+    }
+
+    return new MyOctokit(options.octokitOptions);
+  }
+}
